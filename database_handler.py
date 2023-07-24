6caf24c0b9c5f4e066da24b5482ca55a09a79e54
Maicon-g14 <maicon_gabriel12@hotmail.com>
2023-07-24 16:25:48 -0300

    Improvement in error handling and db insertion

--- database_handler.py
+++ database_handler.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 ''' 
-    A database handler to save currencies scraped into SQLite
+    A database handler to storage scraped currencies into SQLite db
 '''
 
 
@@ -13,12 +13,10 @@ class DatabaseHandler:
         try:
             self.connection = sqlite3.connect(self.db_name)
             self.cursor = self.connection.cursor()
-
-        except Exception as e:
-            print("Error creating the database! " + e.__str__())
-
-        finally:
             return self
+
+        except sqlite3.Error as e:
+            raise RuntimeError(f"Error creating the database! {e}")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.commit()
@@ -27,39 +25,45 @@ class DatabaseHandler:
     def __create_table(self, name):
         try:
             self.cursor.execute(f'''CREATE TABLE IF NOT EXISTS {name} (
-                                    ticker TEXT, 
-                                    date TEXT, 
-                                    open REAL, 
-                                    high REAL, 
-                                    low REAL, 
-                                    close REAL
-                                )''')
+                                        ticker TEXT, 
+                                        date TEXT, 
+                                        open REAL, 
+                                        high REAL, 
+                                        low REAL, 
+                                        close REAL
+                                    )'''
+                                )
             print(f'Table \'{name}\' has been created successfully!')
 
-        except Exception as e:
-            print(f"Error creating \'{name}\' table! " + e.__str__())
+        except sqlite3.Error as e:
+            raise RuntimeError(f"Error creating \'{name}\' table! {e}")
 
     def insert_data(self, data, table_name="currencies"):
         try:
             self.__create_table(table_name)
+
             for item in data:
                 for inner_item in data[item]:
-                    self.cursor.execute(f'''INSERT INTO {table_name} VALUES (
-                                            '{item}', 
-                                            '{inner_item}', 
-                                            {data[item][inner_item][0]}, 
-                                            {data[item][inner_item][1]}, 
-                                            {data[item][inner_item][2]}, 
-                                            {data[item][inner_item][3]}
-                                        )''')
 
-        except Exception as e:
-            print(f'Error inserting data into \'{table_name}\'! ' + e.__str__())
+                    self.cursor.execute(f'''INSERT INTO {table_name} VALUES (
+                                                ?, ?, ?, ?, ?, ?
+                                            )''', (
+                                                item,
+                                                inner_item,
+                                                data[item][inner_item][0],
+                                                data[item][inner_item][1],
+                                                data[item][inner_item][2],
+                                                data[item][inner_item][3]
+                                            )
+                                        )
+
+        except sqlite3.Error as e:
+            print(f'Error inserting data into \'{table_name}\'! {e}')
 
     def get_table(self, table_name="currencies"):
         try:
             self.cursor.execute(f"SELECT * FROM {table_name}")
             return self.cursor.fetchall()
 
-        except Exception as e:
-            print(f'Error fetching data from \'{table_name}\'! ' + e.__str__())
+        except sqlite3.Error as e:
+            print(f'Error fetching data from \'{table_name}\'! {e}')
